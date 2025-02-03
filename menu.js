a3ee873ac4a98d3c9d08cd91c97e8ad4a5edf72a
Rickxxc <117328130+Rickxxc@users.noreply.github.com>
2025-02-03 11:07:26 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -31,7 +31,7 @@ class Menu {
           </div>
           
           <div class="menu-items">
-            <a href="/" class="menu-item">
+            <a href="go:sim" class="menu-item">
               <svg width="24" height="24" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z"/>
               </svg>
